0e1c442ec8bf51daf9a77eeac2156b243403a190
Broc <broc@brocsmith.com>
2022-02-03 14:38:55 -0800

    Minor CSS Change

--- dh-frontend/src/components/ConnectButton.js
+++ dh-frontend/src/components/ConnectButton.js
@@ -8,7 +8,7 @@ const ConnectButton = () => {
 	return (
 		<div className="connect-div">
 		{chainId === 43114 ? <span style={{"display": "none"}} className="info-subtext"></span> : <span className="info-subtext error">Connect to Avax </span> }
-		{account ? <span className="info-subtext">{account.substring(0,8)}…{account.substring(account.length-6)}</span> : <span className="info-subtext error">Not Connected</span>}
+		{account ? <span className="info-subtext">{account.substring(0,8)}…{account.substring(account.length-6)}</span> : <span className="info-subtext">Not Connected</span>}
 		{account ?
 		(
 			<button className="connect-button enableEthereumButton btn btn-primary" onClick={() => deactivate()}>
