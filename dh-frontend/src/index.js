aed26e8b08e4bba586f6ee7d2f23d72ed9a9e3e4
sendicecream <94716930+sendicecream@users.noreply.github.com>
2022-02-04 00:52:59 -0600

    css update

--- dh-frontend/src/index.js
+++ dh-frontend/src/index.js
@@ -78,6 +78,7 @@ export function App() {
                                     <br />
                                       <br />
                                         <center>
+                                            
                                             <MintButton />
                                             <br />
                                               <br />
