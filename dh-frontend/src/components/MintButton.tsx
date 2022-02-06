2983db9aa5db74b71efc01d6ab2eb72b8d568c54
Broc <broc@brocsmith.com>
2022-02-05 18:57:16 -0800

    Finishing touches on MintButton & style.css

--- dh-frontend/src/components/MintButton.tsx
+++ dh-frontend/src/components/MintButton.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../style.css';
-import { useEthers } from '@usedapp/core'
+import { useEthers, useEtherBalance } from '@usedapp/core'
 import { formatEther, formatUnits } from '@ethersproject/units'
 import { utils } from 'ethers'
 import { Contract } from '@ethersproject/contracts'
@@ -13,7 +13,8 @@ const haulersContractAddress = config.address // REPLACE
 const contract = new Contract(haulersContractAddress, haulersInterface)
 
 const MintButton = () => {
-  const { account } = useEthers()
+  const { account, chainId } = useEthers()
+  const avaxBalance = useEtherBalance(account)
   contract.connect(account)
 
   const { state, send } = 
@@ -37,13 +38,19 @@ const MintButton = () => {
 
   return (
     <div className="mint-interface">
-      {(value <= 0) ? <span className="">Invalid number of haulers: {value}</span> : <span style={{"display": "none"}}></span>}
-      <input className="num-to-mint" type="number" id="quantity" name="quantity" onChange={handleInput} min="1" />
-      {state.status == "Mining" ? <span className="">Transaction pending...</span> : <span style={{"display": "none"}}></span>}
-      <span>{state.status}</span>
-      <button className={`sendEthButton btn btn-success`} disabled={!account} onClick={() => {handleClick()}}>
-        Mint
-      </button>
+      {(account && avaxBalance) ? <span className="information-text">Your balance: {formatEther(avaxBalance).substring(0,6)} AVAX</span> : <span style={{"display": "none"}}></span>}
+      <div className="mint-buttons">
+        <input className="num-to-mint" type="number" id="quantity" name="quantity" onChange={handleInput} min="1" />
+        {(account && (chainId === 43114 || chainId === 4) && (state.status != "PendingSignature") && value >= 1)
+          ?
+          <button className={`sendEthButton btn btn-success`} disabled={!account} onClick={() => {handleClick()}}>Mint</button>
+          :
+          <button className={'btn btn-success disabled'} disabled>Mint</button>
+        }
+      </div>
+      {state.status == "PendingSignature" ? <span className="information-text">Transaction pending...</span> : <span style={{"display": "none"}}></span>}
+      {(value <= 0) ? <span className="information-text">Invalid number of haulers: {value}</span> : <span style={{"display": "none"}}></span>}
+      {(chainId === 43114 || chainId === 4) ? <span style={{"display": "none"}}></span> : <span className="information-text">Please connect to the Avalanche Network!</span>}
     </div>
   )
 }
