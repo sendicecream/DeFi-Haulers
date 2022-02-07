f01c4b9eba64fb38603f107c6916266a9c9de690
Broc <broc@brocsmith.com>
2022-02-06 16:45:13 -0800

    Minor change to MintButton.tsx
    Refactor + minor functionality change

--- dh-frontend/src/components/MintButton.tsx
+++ dh-frontend/src/components/MintButton.tsx
@@ -14,6 +14,7 @@ const contract = new Contract(haulersContractAddress, haulersInterface)
 const MintButton = () => {
   const { account, chainId } = useEthers()
   const avaxBalance = useEtherBalance(account)
+  const correctChainId = (chainId === 43114 || chainId === 4)
   contract.connect(account)
 
   const { state, send } = 
@@ -37,10 +38,10 @@ const MintButton = () => {
 
   return (
     <div className="mint-interface">
-      {(account && avaxBalance) ? <span className="information-text">Your balance: {formatEther(avaxBalance).substring(0,6)} AVAX</span> : <span style={{"display": "none"}}></span>}
+      {(account && avaxBalance && correctChainId) ? <span className="information-text">Your balance: {formatEther(avaxBalance).substring(0,6)} AVAX</span> : <span style={{"display": "none"}}></span>}
       <div className="mint-buttons">
         <input className="num-to-mint" type="number" id="quantity" name="quantity" onChange={handleInput} min="1" />
-        {(account && avaxBalance && (chainId === 43114 || chainId === 4) && (state.status != "PendingSignature") && value >= 1 && (formatEther(avaxBalance) >= value))
+        {(account && avaxBalance && correctChainId && (state.status != "PendingSignature") && value >= 1 && (formatEther(avaxBalance) >= value))
           ?
           <button className={`sendEthButton btn btn-success`} disabled={!account} onClick={() => {handleClick()}}>Mint</button>
           :
@@ -49,7 +50,7 @@ const MintButton = () => {
       </div>
       {state.status == "PendingSignature" ? <span className="information-text">Transaction pending...</span> : <span style={{"display": "none"}}></span>}
       {(value <= 0) ? <span className="information-text">Invalid number of haulers: {value}</span> : <span style={{"display": "none"}}></span>}
-      {(chainId === 43114 || chainId === 4) ? <span style={{"display": "none"}}></span> : <span className="information-text">Please connect to the Avalanche Network!</span>}
+      {correctChainId ? <span style={{"display": "none"}}></span> : <span className="information-text">Please connect to the Avalanche Network!</span>}
     </div>
   )
 }
